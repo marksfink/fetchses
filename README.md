50031068495521eddcfcbd9c128d1c93a28b4283
marksfink <github@finkbox.net>
2025-02-01 16:49:05 +0000

    AWS SDK v2 + S3 encryption SDK v3

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-The use-case is to receive email for my custom domain with AWS's SES service, which encrypts the email and writes it to an S3 bucket.  SES uses client-side encryption and while that is probably a good thing, AWS provides no way to decrypt the messages other than with the AWS SDK for Go, Java, or Ruby.  Hence, this program.
+The use-case for this is to receive email for my personal domain with AWS's SES service, which encrypts the email and writes it to an S3 bucket.  SES uses client-side encryption and while that is probably a good thing, AWS provides no way to decrypt the messages other than with the AWS SDK for Go, Java, or Ruby.  Hence, this program.
 
-I run fetchses locally to fetch and decrypt the email and send it to a local SMTP server (that passes it on to a local IMAP server).  Since it pulls from S3, I don't need a static IP address or open inbound ports for the SMTP server.  The server can be completely private.  It is written with this use-case in mind.  Once emails are fetched and decrypted, the program trusts the local environment.  Be mindful of that if you use it.
+I run fetchses locally to fetch and decrypt the email and send it to a local SMTP server (that passes it on to a local IMAP server).  The SMTP and IMAP servers can be completely private and it is written with this use-case in mind.  Once emails are fetched and decrypted, the program trusts the local environment.
 
-The larger point of this is that I can run my own private email server and let AWS deal with DKIM, SPF, DMarc, IP reputation, spam/virus scanning, and the security challenges of running a publicly-exposed SMTP server.
+The larger point of this is that I can run a private email server and let AWS deal with the security challenges of running a publicly-exposed SMTP server.
 
 I added comments to fetchses.yml.example that should be sufficient for using the program.  You need a properly configured SMTP server to go with it.  The program takes up to three command-line arguments:
 - the path to the config file (it defaults to ~/.config/fetchses.yml).
 - the S3 bucket storing the incoming email.  This can also be set in the config file.  The argument overrides the config file.
-- an S3 file name (including prefix) for a specific email.  If this is not set, the program will list/read all emails in the configured bucket and prefix.
+- an S3 file name (including prefix) for a specific email.  If this is not set, the program will retrieve all emails in the configured bucket and prefix.
 
-I added the bucket and key arguments to go with SNS notifications for new mail.  The SNS notifications contain the bucket and key for new mail, so I use that to fetch those specific files in near-real-time as the mail arrives.  It is still useful to list and read all files if something goes wrong.  You can run it manually then and fetch whatever is there.
+I added the bucket and key arguments to go with SNS notifications for new mail.  The SNS notifications contain the bucket and key for new mail, so I use that to fetch those specific files in near-real-time as the mail arrives.  It is still useful to retrieve all files if something goes wrong.
 
-As a rule, I left out anything that I can otherwise do with the SMTP and IMAP servers, like aliases, sieve rules, and spam/virus scanning.  That said, I have chosen (for now) to use SES's spam and virus scanning.  SES adds headers indicating PASS or FAIL for each.  fetchses checks the SES virus header and sends an alert email to me if the header is FAIL.  I also have postfix check for that header and quarantine the email.  However postfix will not notify me of it.  So fetchses notifies me.  I use a sieve rule to deal with the SES spam header.
-
-I don't anticipate doing much more with fetchses.  I'll address bugs and CVEs of course and otherwise stay in step with updates to the AWS SDK for Go, SES, and S3.   I hope this is infrequent.
+As a rule, I left out anything that I can otherwise do with the SMTP and IMAP servers, like aliases, sieve rules, and spam/virus scanning.  That said, I have chosen (for now) to use SES's spam and virus scanning.  SES adds headers indicating PASS or FAIL for each.  fetchses checks the SES virus header and sends an alert email to me if the header is FAIL.  I also have postfix check for that header and quarantine the email.  However postfix will not notify me of it (thus I baked it into fetchses).  I use a sieve rule to deal with the SES spam header.
--- aws.go
+++ aws.go
@@ -1,70 +1,65 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"strings"
 
-	// TODO: https://github.com/aws/aws-sdk-go-v2/issues/1636
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3crypto"
+	crypto "github.com/aws/amazon-s3-encryption-client-go/v3/client"
+	"github.com/aws/amazon-s3-encryption-client-go/v3/materials"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-type s3Config struct {
+type s3Session struct {
 	Profile       string `yaml:"profile"`
 	Bucket        string `yaml:"bucket"`
 	NewMailPrefix string `yaml:"newMailPrefix"`
 	ErrorPrefix   string `yaml:"errorPrefix"`
-	MasterKeyId   string `yaml:"masterKeyId"`
-	client        *s3.S3
-	decryptClient *s3crypto.DecryptionClientV2
+	KmsKeyId      string `yaml:"kmsKeyId"`
+	ctx           context.Context
+	client        *s3.Client
+	decrypt       *crypto.S3EncryptionClientV3
 	key           string
 }
 
-func (s3Cfg *s3Config) connectS3() error {
-	sess, err := session.NewSessionWithOptions(session.Options{
-		Profile:           s3Cfg.Profile,
-		SharedConfigState: session.SharedConfigEnable,
-	})
+func (sess *s3Session) connectS3() error {
+	cfg, err := awsconfig.LoadDefaultConfig(sess.ctx,
+		awsconfig.WithSharedConfigProfile(sess.Profile))
 	if err != nil {
-		return fmt.Errorf("failed to initialize AWS session: %v", err)
+		return fmt.Errorf("failed to load AWS config, %v", err)
 	}
-	s3Cfg.client = s3.New(sess)
-	s3Cfg.decryptClient, err = initDecryptionClient(sess, s3Cfg.MasterKeyId)
+	sess.client = s3.NewFromConfig(cfg)
+	// EnableLegacyWrappingAlgorithms is required with objects that SES writes.
+	cmm, err := materials.NewCryptographicMaterialsManager(
+		materials.NewKmsKeyring(kms.NewFromConfig(cfg), sess.KmsKeyId,
+			func(options *materials.KeyringOptions) {
+				options.EnableLegacyWrappingAlgorithms = true
+			},
+		),
+	)
 	if err != nil {
-		return fmt.Errorf("failed to initialize S3 decryption client: %v", err)
+		return fmt.Errorf("failed to initialize cmm, %v", err)
+	}
+	sess.decrypt, err = crypto.New(sess.client, cmm)
+	if err != nil {
+		return fmt.Errorf("failed to initialize encryption client, %v", err)
 	}
 	return nil
 }
 
-func initDecryptionClient(sess *session.Session, masterKeyId string) (*s3crypto.DecryptionClientV2, error) {
-	cr := s3crypto.NewCryptoRegistry()
-	if err := s3crypto.RegisterAESGCMContentCipher(cr); err != nil {
-		return nil, err
-	}
-	// RegisterKMSWrapWithCMK is deprecated, but the current function
-	// RegisterKMSContentWrapWithCMK fails with SES-encrypted files.
-	// This is likely to change and we'll need to update this.
-	if err := s3crypto.RegisterKMSWrapWithCMK(cr, kms.New(sess), masterKeyId); err != nil {
-		return nil, err
-	}
-	svc, err := s3crypto.NewDecryptionClientV2(sess, cr)
+func (sess *s3Session) listNewMail() ([]string, error) {
+	result, err := sess.client.ListObjectsV2(sess.ctx,
+		&s3.ListObjectsV2Input{
+			Bucket: aws.String(sess.Bucket),
+			Prefix: aws.String(sess.NewMailPrefix),
+		},
+	)
 	if err != nil {
-		return nil, err
-	}
-	return svc, nil
-}
-
-func (s3Cfg *s3Config) listNewMail() ([]string, error) {
-	result, err := s3Cfg.client.ListObjectsV2(&s3.ListObjectsV2Input{
-		Bucket: aws.String(s3Cfg.Bucket),
-		Prefix: aws.String(s3Cfg.NewMailPrefix),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to list objects in %s: %v", s3Cfg.Bucket, err)
+		return nil, fmt.Errorf("failed to list objects in %s: %v", sess.Bucket, err)
 	}
 	var keys []string
 	for i := 0; i < int(*result.KeyCount); i++ {
@@ -73,21 +68,31 @@ func (s3Cfg *s3Config) listNewMail() ([]string, error) {
 	return keys, nil
 }
 
-func (s3Cfg *s3Config) decryptMail() ([]byte, error) {
+func (sess *s3Session) decryptMail() ([]byte, error) {
 	var msg []byte
-	result, err := s3Cfg.decryptClient.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(s3Cfg.Bucket),
-		Key:    aws.String(s3Cfg.key),
-	})
+
+	// In 1/2025, AWS started validating checksums by default when getting
+	// objects. SES does not include checksums when it writes to S3, so we get
+	// a warning about it when getting the object. The option below disables
+	// the check so we don't get the warning.
+	result, err := sess.decrypt.GetObject(sess.ctx,
+		&s3.GetObjectInput{
+			Bucket: aws.String(sess.Bucket),
+			Key:    aws.String(sess.key),
+		},
+		func(options *s3.Options) {
+			options.ResponseChecksumValidation = aws.ResponseChecksumValidationWhenRequired
+		},
+	)
 	if err == nil {
 		msg, err = io.ReadAll(result.Body)
 	}
 	if err != nil {
-		_, filename, _ := strings.Cut(s3Cfg.key, "/")
+		_, filename, _ := strings.Cut(sess.key, "/")
 		err = fmt.Errorf("failed to decrypt S3 object: %v\n"+
-			"moving to %s/%s/%s", err, s3Cfg.Bucket,
-			s3Cfg.ErrorPrefix, filename)
-		if err2 := s3Cfg.moveUndeliveredMail(filename); err2 != nil {
+			"moving to %s/%s/%s", err, sess.Bucket,
+			sess.ErrorPrefix, filename)
+		if err2 := sess.moveUndeliveredMail(filename); err2 != nil {
 			// Staying with %w for now versus errors.Join() because
 			// Unwrap does not work with Join presently.
 			err = fmt.Errorf("%w\n%v", err, err2)
@@ -96,23 +101,27 @@ func (s3Cfg *s3Config) decryptMail() ([]byte, error) {
 	return msg, err
 }
 
-func (s3Cfg *s3Config) moveUndeliveredMail(filename string) error {
-	_, err := s3Cfg.client.CopyObject(&s3.CopyObjectInput{
-		Bucket:     aws.String(s3Cfg.Bucket),
-		CopySource: aws.String(s3Cfg.Bucket + "/" + s3Cfg.key),
-		Key:        aws.String(s3Cfg.ErrorPrefix + "/" + filename),
-	})
+func (sess *s3Session) moveUndeliveredMail(filename string) error {
+	_, err := sess.client.CopyObject(sess.ctx,
+		&s3.CopyObjectInput{
+			Bucket:     aws.String(sess.Bucket),
+			CopySource: aws.String(sess.Bucket + "/" + sess.key),
+			Key:        aws.String(sess.ErrorPrefix + "/" + filename),
+		},
+	)
 	if err != nil {
 		return fmt.Errorf("failed to copy S3 object: %v", err)
 	}
-	return s3Cfg.deleteObject()
+	return sess.deleteObject()
 }
 
-func (s3Cfg *s3Config) deleteObject() error {
-	_, err := s3Cfg.client.DeleteObject(&s3.DeleteObjectInput{
-		Bucket: aws.String(s3Cfg.Bucket),
-		Key:    aws.String(s3Cfg.key),
-	})
+func (sess *s3Session) deleteObject() error {
+	_, err := sess.client.DeleteObject(sess.ctx,
+		&s3.DeleteObjectInput{
+			Bucket: aws.String(sess.Bucket),
+			Key:    aws.String(sess.key),
+		},
+	)
 	if err != nil {
 		err = fmt.Errorf("failed to delete S3 object: %v", err)
 	}
